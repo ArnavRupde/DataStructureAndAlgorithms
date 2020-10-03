38eacd7c9d6fed99a0dc44aef5b76ab11e474281
Raj Shah <rajshah9914@gmail.com>
2020-10-03 16:04:21 +0530

    Dynamic Programming

---
+++ dpOnCounts.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int rob(vector<int> &nums)
+    {
+        int n = nums.size();
+        int dp[100001] = {0};
+        if (n < 1)
+            return 0;
+        if (n < 2)
+            return nums[0];
+        dp[0] = nums[0];
+        dp[1] = max(dp[0], nums[1]);
+        for (int i = 2; i < n; i++)
+        {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return *max_element(dp, dp + n);
+    }
+};
